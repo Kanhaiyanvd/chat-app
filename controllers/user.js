873ae1717e1f84399fbfe7bb152df5c040cb0e40
Kanhaiyanvd <118625121+Kanhaiyanvd@users.noreply.github.com>
2023-03-21 22:39:54 +0530

    CORs

--- controllers/user.js
+++ controllers/user.js
@@ -11,19 +11,24 @@ function isstringinvalid(string){
 }
 exports.signup = async(req,res,next)=>{
  try{
-    //const {name, email, phonenumber, password} = req.body;
-    console.log(req.body)
     const {name, email, phonenumber, password} = req.body;
     if(isstringinvalid(name) || isstringinvalid(email) || isstringinvalid(phonenumber) || isstringinvalid(password)){
         return res.status(400).json({error:"All fields are required"})
     }
-    const saltround = 10;
-    console.log('hii')
-    bcrypt.hash(password, saltround, async(err, hash)=>{
+    const signupuser = await User.findAll({where: {email}})
+    if(signupuser.length>0){
+        console.log(signupuser);
+        res.status(401).json({message: 'User already exits, please login'});
+    }
+    else{
+     const saltround = 10;
+    
+     bcrypt.hash(password, saltround, async(err, hash)=>{
         console.log(err);
         await User.create({name, email, phonenumber, password: hash})
         res.status(201).json({message: 'User created successfully'})
-    })
+     })
+    }
 
  }catch(err){
    console.log(err)
